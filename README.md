a255fa00a40f4df2683366405f7c069d63425f4b
Upd4ting <upd4ting@gmail.com>
2020-09-17 12:30:17 +0200

    first release

---
+++ README.md
@@ -0,0 +1,28 @@
+# Nativescript Hook Versioning
+
+[![npm](https://img.shields.io/npm/v/nativescript-hook-versioning)]
+
+This plugin add an hook that do the followings things:
+
+- Take your `version` from `nativescript.config.ts` and put it as `versionName` into your `AndroidManifest.xml` and as `CFBundleShortVersionString` into your `Info.plist`.
+- Take an environment variable of your choice and put it as `versionCode` into your `AndroidManifest.xml` and as `CFBundleVersion` into your `Info.plist`. That allow you to use an environment variable from your CICD and auto increment your version code.
+
+# Installation
+
+`ns plugin add nativescript-hook-versioning`
+
+# Usage
+
+You can add the following configuration into your `nativescript.config.ts`
+
+```
+nativescriptHookVersioning: {
+    versionName: true,
+    versionCode: {
+      enabled: true,
+      content: 'BUNDLE_VERSION_CODE',
+    },
+  },
+```
+
+to configure it to your likings.
---
+++ src/after-prepare.js
@@ -0,0 +1,74 @@
+const fs = require("fs");
+const AndroidManifest = require("androidmanifest");
+const iOSPList = require("plist");
+
+module.exports = function (
+  $logger,
+  $platformsDataService,
+  $projectData,
+  hookArgs
+) {
+  const nsConfig = $projectData.nsConfig;
+  const hookConfig = $projectData.nsConfig.nativescriptHookVersioning;
+
+  if (!nsConfig || !nsConfig.version) {
+    $logger.warn(
+      "[nativescript-hook-versioning] Nativescript version is not defined. Skipping set native package version."
+    );
+    return;
+  }
+
+  if (!hookConfig) {
+    $logger.warn(
+      "[nativescript-hook-versioning] The hook config isn't defined."
+    );
+    return;
+  }
+
+  const platform = hookArgs.prepareData.platform;
+  const platformData =
+    $platformsDataService.platformsDataService[platform]._platformData;
+
+  if (platform == "android") {
+    let manifest = new AndroidManifest().readFile(
+      platformData.configurationFilePath
+    );
+
+    if (hookConfig.versionName) {
+      manifest.$("manifest").attr("android:versionName", nsConfig.version);
+    }
+
+    if (
+      hookConfig.versionCode &&
+      hookConfig.versionCode.enabled &&
+      process.env[hookConfig.versionCode.content]
+    ) {
+      manifest
+        .$("manifest")
+        .attr(
+          "android:versionCode",
+          process.env[hookConfig.versionCode.content]
+        );
+    }
+
+    manifest.writeFile(platformData.configurationFilePath);
+  } else if (platform == "ios") {
+    let plist = iOSPList.parse(
+      fs.readFileSync(platformData.configurationFilePath, "utf8")
+    );
+
+    if (hookConfig.versionName) {
+      plist.CFBundleShortVersionString = nsConfig.version;
+    }
+
+    if (
+      hookConfig.versionCode &&
+      hookConfig.versionCode.enabled &&
+      process.env[hookConfig.versionCode.content]
+    ) {
+      plist.CFBundleVersion = process.env[hookConfig.versionCode.content];
+    }
+
+    fs.writeFileSync(platformData.configurationFilePath, iOSPList.build(plist));
+  }
+};
